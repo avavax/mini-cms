724258f04bd2d413dbb1ca8bde6f842bb3101470
avavax <ip-media@yandex.ru>
2020-09-01 20:03:18 +0300

    modify spl_autoload

--- bootstrap.php
+++ bootstrap.php
@@ -5,16 +5,9 @@ define('VIEW_DIR', '/src/App/View/');
 
 spl_autoload_register(function(string $class) {
 
-    $prefix = '';
     $baseDir = __DIR__ . APP_DIR;
 
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-
-    $relativeClass = substr($class, $len);
-    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+    $file = $baseDir . str_replace('\\', '/', $class) . '.php';
     if (file_exists($file)) {
         include $file;
     }
